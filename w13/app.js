904b933f978fafdf355ec7b5e0cd3258c9ccd4ba
timislaw <personal.timross@gmail.com>
2022-04-06 23:57:08 -0700

    setting up functions

---
+++ w13/app.js
@@ -0,0 +1,7 @@
+document.querySelector("#orders").addEventListener("submit", () => {
+  const generateOrders = new Orders();
+
+  getOrders();
+});
+
+console.log(randomNumber());
---
+++ w13/functions.js
@@ -0,0 +1,1 @@
+const randomNumber = (min = 1, max = 5) => Math.floor(Math.random() * (max - min + 1) + min);
