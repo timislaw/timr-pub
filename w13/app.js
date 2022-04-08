72a9684e954dcf54ee09872f09ef8ff1f32ca6df
timislaw <personal.timross@gmail.com>
2022-04-07 22:12:49 -0700

    was stuck figuring out AGAIN why my random number was sticking with the same number. I realized what I was doing and was setting the price AGAIN to a set number generated. So i fixed that, haha.

--- w13/app.js
+++ w13/app.js
@@ -1,7 +1,48 @@
-document.querySelector("#orders").addEventListener("submit", () => {
-  const generateOrders = new Orders();
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-  getOrders();
+let amount = 0;
+const items = ['Hawaiian Shirt', 'Poke Bowl', 'Spam Musubi', 'Snorkel Rental'];
+let price = 0;
+
+
+
+
+const Order = function (item, price) {
+    this.item = item;
+    this.price = price;
+};
+
+Order.prototype.yourOrder = function () {
+    return `You ordered a ${this.item} for $${this.price}.`
+
+};
+
+Order.prototype.yourAmount = function () {
+    amount = amount + this.price;
+    return amount;
+};
+
+const renderOrder = (order) => {
+    const head = document.createElement('h3');
+    const paragraph = document.createElement('p');
+    const spacer = document.createElement('br');
+    paragraph.textContent = order.yourOrder();
+    document.querySelector('#output').appendChild(paragraph);
+    document.querySelector('#output').appendChild(spacer);
+    document.querySelector('#amount').innerHTML = '';
+    head.textContent = `Your amount is $${order.yourAmount()}`;
+    document.querySelector('#amount').appendChild(head);
+
+};
+
+
+
+document.querySelector("#orders").addEventListener("click", (e) => {
+  let item = items[randomNumber(0, 3)];
+  let prices = price + randomNumber(1, 25)
+  console.log(item, prices);
+  const orders = new Order(item, prices);
+  renderOrder(orders);
+  e.preventDefault();
+
 });
-
-console.log(randomNumber());
